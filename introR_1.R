5361e47d80babcdffae59a4446ec3be6a3ce962c
Nicole Leung <nleung89@gmail.com>
2016-04-14 16:19:13 -0700

    Suffered through loops

--- introR_1.R
+++ introR_1.R
@@ -1,35 +1,35 @@
-#ANALYZING RAPTOR DATA
-#loading data
+# ANALYZING RAPTOR DATA
+# loading data
 setwd("~/Desktop/software-carpentry-2016/data-files/r-lesson/data/")
 list.files()
 read.csv(file = "inflammation-01.csv", header = FALSE)
 
-#assigning variables
+# assigning variables
 weight_kg <- 55
 weight_kg
-  #weight in pounds
+  # weight in pounds
 2.2 * weight_kg
 weight_kg <- 57.5
 weight_kg
-  #calculate weight in pounds, save to variable weight_lb
+  # calculate weight in pounds, save to variable weight_lb
 weight_lb <- 2.2 * weight_kg
 weight_lb
 
-#manipulating data
+# manipulating data
 dat <- read.csv(file = "inflammation-01.csv", header = FALSE)
 head(dat)
 class(dat)
 dim(dat)
-  #first value in dat
+  # first value in dat
 dat[1,1]
-  #middle value in dat
+  # middle value in dat
 dat[30,20]
 dat[1:4, 1:10]
 dat[5:10, 1:10]
 dat[c(3,8,37,56), c(10,14,29)]
-  #all columns from row 5
+  # all columns from row 5
 dat[5,]
-  #all rows from row 16
+  # all rows from row 16
 dat[,16]
 
 raptor_1 <- dat[1,]
@@ -48,12 +48,12 @@ avg_raptor_inflammation
 avg_day_inflammation <- apply(dat,2,mean)
 avg_day_inflammation
 
-#same as apply(dat,1,mean)
+# same as apply(dat,1,mean)
 rowMeans(dat)
-#same as apply(dat,2,mean
+# same as apply(dat,2,mean
 colMeans(dat)
 
-#challenge-slicing data
+# challenge-slicing data
 animal <- c("m", "o", "n", "k", "e", "y")
 animal[1:3]
 animal[4:6]
@@ -62,10 +62,10 @@ animal[4:1]
 animal[-1]
 animal[-4]
 animal[-1:-4]
-#specify individual values in a list
+# specify individual values in a list
 animal[c(5,2,3)]
 
-#plotting
+# plotting
 plot(avg_day_inflammation)
 max_day_inflammation <- apply(dat, 2, max)
 plot(max_day_inflammation)
@@ -74,15 +74,16 @@ plot(min_day_inflammation)
 sd_day_inflammation <- apply(dat, 2, sd)
 plot(sd_day_inflammation)
 
-#CREATING FUNCTIONS
-#defining a function: fahrenheit to kelvin
+
+# CREATING FUNCTIONS
+# defining a function: fahrenheit to kelvin
 fahr_to_kelvin <-function(temp){
   kelvin <-((temp-32)*(5/9)) + 273.15
   return(kelvin)
 }
 fahr_to_kelvin(100)
                
-#composing functions  
+# composing functions  
 kelvin_to_celsius <- function(temp) {
   celsius <- temp - 273.15
   return(celsius)
@@ -104,4 +105,117 @@ fence <- function(original,wrapper) {
 fence(original,wrapper)
 
 
+# ANALZING MULTIPLE DATA SETS
+list.files()
+analyze <- function(filename) {
+  # Plots the average, min, and max inflammation over time.
+  # Input is character string of a csv file.
+  
+  dat <- read.csv(file = filename, header = FALSE)
+  avg_day_inflammation <- apply(dat, 2, mean)
+  plot(avg_day_inflammation)
+  max_day_inflammation <- apply(dat, 2, max)
+  plot(max_day_inflammation)
+  min_day_inflammation <- apply(dat, 2, min)
+  plot(min_day_inflammation)
+}
 
+analyze("inflammation-01.csv")
+analyze("inflammation-02.csv")
+
+# For loops
+# Bad approach example
+best_practice <- c("Let", "the", "computer", "do", "the", "work")
+print_words <- function(sentence) {
+  print(sentence[1])
+  print(sentence[2])
+  print(sentence[3])
+  print(sentence[4])
+  print(sentence[5])
+  print(sentence[6])
+}
+
+print_words(best_practice)
+print_words(best_practice[-6])
+
+# Good approach example
+print_words <- function(sentence) {
+  for (word in sentence) {
+    # word <- sentence[1]
+    # word <- sentence[2]
+    print(word)
+  }
+}
+
+print_words(best_practice)
+print_words(best_practice[-6])
+
+# for(variable in collection) {
+# do things with variable
+# }
+
+# Another example
+len <- 0
+vowels <- c("a", "e", "i", "o", "u")
+for (v in vowels) {
+  print(len)
+  len <- len + 1
+}
+# Number of vowels
+len
+
+# Challenge - using loops
+## Question 1
+seq(3)
+print_N <- function(natural_num) {
+  # Function prints natural numbers
+  # Input is a number
+for (x in seq(natural_num)) 
+  print(x)
+}
+print_N(3)
+print_N(7)
+
+## Question 2
+ex_vec <- c(4, 8, 15, 16, 23, 42)
+total <- function(num) {
+  tot = 0
+  for(x in num) {
+    tot = tot + x
+  }
+  return(tot)
+}
+total(ex_vec)
+  
+## Question 3
+2^4
+expo <- function(base, exponent) {
+  tot = 1
+  # Accounts for exponent = 0. Don't worry about this for now
+  if(exponent == 0){
+    return(tot)
+  }
+  
+  for(i in 1:exponent){
+    tot = tot * base
+  }
+  return(tot)
+}
+
+expo(3, 0)
+expo(3, 2)
+
+# Processing multiple flies - loop through files analysis
+list.files()
+# Allows to use the wildcard
+Sys.glob("*csv")
+Sys.glob("inflammation***.csv")
+# This also works
+list.files(pattern="inflammation")
+
+filenames <- Sys.glob("i*.csv")
+  for (file in filenames) {
+    print(file)
+    analyze(file)
+  }
+  
